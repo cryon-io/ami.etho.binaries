7db660533cbd889a75801e5836fe779afa9ba450
cryi <cryi@tutanota.com>
2020-02-21 20:45:45 +0100

    fixed file permissions

--- src/__etho/download-binaries.lua
+++ src/__etho/download-binaries.lua
@@ -10,12 +10,12 @@ local function download_and_extract(url, dst, options)
     local _ok, _error = eliNet.safe_download_file(url, _tmpFile, {followRedirects = true})
     if not _ok then
         eliFs.remove(_tmpFile)
-        ami_error(_exString.join_strings("Failed to download: ", _error), EXIT_APP_DOWNLOAD_ERROR)
+        ami_error("Failed to download: " .. tostring(_error), EXIT_APP_DOWNLOAD_ERROR)
     end
 
     local _ok, _error = eliZip.safe_extract(_tmpFile, dst, options)
     eliFs.remove(_tmpFile)
-    ami_assert(_ok, _exString.join_strings("Failed to extract: ", _error), EXIT_APP_DOWNLOAD_ERROR)
+    ami_assert(_ok, "Failed to extract: " .. tostring(_error), EXIT_APP_DOWNLOAD_ERROR)
 end
 
 log_info"Downloading geth..."
@@ -30,7 +30,7 @@ ami_assert(_ok, "Failed to enumerate binaries", EXIT_APP_IO_ERROR)
 
 for _, file in ipairs(_files) do 
     local _path = eliPath.combine("bin", file)
-    local _ok, _error = eliFs.safe_chmod(file, "r-xr-xr-x")
+    local _ok, _error = eliFs.safe_chmod(file, "rwxrwxrwx")
     if not _ok then 
         ami_error("Failed to set file permissions for " .. _path .. " - " .. _error, EXIT_APP_IO_ERROR)
     end
