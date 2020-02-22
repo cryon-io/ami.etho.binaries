5eb2d1704e17329b1900436887607a41eb38e232
cryi <cryi@tutanota.com>
2020-02-22 08:44:20 +0100

    fixed permission configuration

--- src/__etho/download-binaries.lua
+++ src/__etho/download-binaries.lua
@@ -25,13 +25,14 @@ download_and_extract(APP.model.ETHOFS_URL, "bin", {flattenRootDir = true, openFl
 log_info"Downloading ipfs..."
 download_and_extract(APP.model.IPFS_URL, "bin", {flattenRootDir = true, openFlags = 0})
 
-local _ok, _files =eliFs.safe_read_dir("bin")
+local _ok, _files = eliFs.safe_read_dir("bin", { returnFullPaths = true})
 ami_assert(_ok, "Failed to enumerate binaries", EXIT_APP_IO_ERROR)
 
 for _, file in ipairs(_files) do 
-    local _path = eliPath.combine("bin", file)
-    local _ok, _error = eliFs.safe_chmod(file, "rwxrwxrwx")
-    if not _ok then 
-        ami_error("Failed to set file permissions for " .. _path .. " - " .. _error, EXIT_APP_IO_ERROR)
+    if eliFs.file_type(file) == 'file' then 
+        local _ok, _error = eliFs.safe_chmod(file, "rwxrwxrwx")
+        if not _ok then 
+            ami_error("Failed to set file permissions for " .. _path .. " - " .. _error, EXIT_APP_IO_ERROR)
+        end
     end
 end
