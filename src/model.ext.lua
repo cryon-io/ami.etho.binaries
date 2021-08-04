77d07d5296d3d3a6d75317a13e1d3e3548e6210e
cryi <cryi@tutanota.com>
2021-08-04 19:36:14 +0200

    updated to etho 1.5.0

--- src/model.ext.lua
+++ src/model.ext.lua
@@ -1,6 +1,6 @@
 am.app.set_model(
     {
-        DAEMON_URL = "https://github.com/Ether1Project/Ether1/releases/download/V1.4.5/ether-1-linux-1.4.5.tar.gz"
+        DAEMON_URL = "https://github.com/Ether1Project/Ether1/releases/download/V1.5.0/etho-linux-1.5.0.tar.gz"
     },
     {merge = true, overwrite = true}
 )
