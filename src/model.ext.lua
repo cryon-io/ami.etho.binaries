1b1c62f2408e8ea92ff7f4ffef615529331fab6b
cryi <cryi@tutanota.com>
2021-08-28 21:50:33 +0200

    added VERSIONS

--- src/model.ext.lua
+++ src/model.ext.lua
@@ -1,6 +1,9 @@
 am.app.set_model(
     {
-        DAEMON_URL = "https://github.com/Ether1Project/Ether1/releases/download/V1.5.0/etho-linux-1.5.0.tar.gz"
+        DAEMON_URL = "https://github.com/Ether1Project/Ether1/releases/download/V1.5.0/etho-linux-1.5.0.tar.gz",
+        VERSIONS = {
+            geth = "1.5.0"
+        }
     },
     {merge = true, overwrite = true}
 )
