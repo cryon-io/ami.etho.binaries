f9b181462438ddbe3fe72f49e21aa49fc23bb546
cryi <cryi@tutanota.com>
2021-10-15 19:59:20 +0200

    updated to atlas 1.5.1

--- src/model.ext.lua
+++ src/model.ext.lua
@@ -1,8 +1,8 @@
 am.app.set_model(
     {
-        DAEMON_URL = "https://github.com/Ether1Project/Ether1/releases/download/V1.5.0/etho-linux-1.5.0.tar.gz",
+        DAEMON_URL = "https://github.com/Ether1Project/Ether1/releases/download/V1.5.1/etho-linux-1.5.1.tar.gz",
         VERSIONS = {
-            geth = "1.5.0"
+            geth = "1.5.1"
         }
     },
     {merge = true, overwrite = true}
