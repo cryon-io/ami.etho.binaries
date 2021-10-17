64485d66ca6894e2bf9e882eab2d02499a48995c
cryi <cryi@tutanota.com>
2021-10-17 22:17:37 +0200

    update to 1.5.2

--- src/model.ext.lua
+++ src/model.ext.lua
@@ -1,8 +1,8 @@
 am.app.set_model(
     {
-        DAEMON_URL = "https://github.com/Ether1Project/Ether1/releases/download/V1.5.1/etho-linux-1.5.1.tar.gz",
+        DAEMON_URL = "https://github.com/Ether1Project/Ether1/releases/download/V1.5.2/etho-linux-1.5.2.tar.gz",
         VERSIONS = {
-            geth = "1.5.1"
+            geth = "1.5.2"
         }
     },
     {merge = true, overwrite = true}
